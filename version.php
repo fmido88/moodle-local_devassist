2ceb3a387cf5288beb4121a15156e818e88ac70b
Mohammad Farouk <phun.for.physics@gmail.com>
2024-12-18 15:33:36 +0200

    `Update version information in local_devassist plugin`

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_devassist';
-$plugin->release = '1.5.0';
-$plugin->version = 2024062200;
+$plugin->release = '1.5.1';
+$plugin->version = 2024121800;
 $plugin->requires = 2022112800;
-$plugin->maturity = MATURITY_STABLE;
+$plugin->maturity = MATURITY_BETA;
