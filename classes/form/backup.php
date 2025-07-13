cefc88357ba9e67ae9be2c543bce5e912d7d9d93
Mohammad Farouk <phun.for.physics@gmail.com>
2025-07-14 02:21:08 +0300

    Adding advanced options in the backup

--- classes/form/backup.php
+++ classes/form/backup.php
@@ -17,6 +17,7 @@
 namespace local_devassist\form;
 
 use local_devassist\local\backup\backup_base;
+use local_devassist\local\backup\backup_database_tables;
 
 defined('MOODLE_INTERNAL') || die();
 global $CFG;
@@ -35,12 +36,48 @@ class backup extends \moodleform {
      * @return void
      */
     protected function definition() {
-        global $OUTPUT;
+        global $OUTPUT, $DB;
         $mform = $this->_form;
 
         $options = backup_base::get_options();
         $mform->addElement('select', 'type', get_string('backup_type', 'local_devassist'), $options);
         $mform->addHelpButton('type', 'backup_type', 'local_devassist');
+
+        $mform->addElement('text', 'chunksize', get_string('chunksize', 'local_devassist'));
+        $mform->setType('chunksize', PARAM_ALPHANUM);
+        $mform->addHelpButton('chunksize', 'chunksize', 'local_devassist');
+
+        $defaultchunk = backup_base::get_max_upload_size(false);
+        $mform->setDefault('chunksize', $defaultchunk);
+
+        $mform->addElement('header', 'advanced', get_string('advancebackup', 'local_devassist'));
+        $mform->setAdvanced('advanced');
+
+        $tables = $DB->get_tables();
+        $options = array_combine($tables, $tables);
+
+        $select = $mform->addElement('select', 'ignoredtables', get_string('ignoredtables', 'local_devassist'), $options);
+        $select->setMultiple(true);
+        $mform->addHelpButton('ignoredtables', 'ignoredtables', 'local_devassist');
+        $mform->hideIf('ignoredtables', 'type', 'neq', 'database_tables');
+        $backer = new backup_database_tables(false, false);
+        $mform->setDefault('ignoredtables', $backer->get_ignored_tables());
+        $mform->setAdvanced('ignoredtables');
+
+        $mform->addElement('textarea', 'ignoredfileareas', get_string('ignoredfileareas', 'local_devassist'));
+        $mform->setType('ignoredfileareas', PARAM_TEXT);
+        $mform->addHelpButton('ignoredfileareas', 'ignoredfileareas', 'local_devassist');
+        $mform->hideIf('ignoredfileareas', 'type', 'eq', 'plugins');
+        $mform->setDefault('ignoredfileareas', $backer->get_excluded_fileareas(true));
+        $mform->setAdvanced('ignoredfileareas');
+
+        $mform->addElement('static', 'instruction',
+                            get_string('instruction', 'local_devassist'),
+                            get_string('backup_instruction', 'local_devassist'));
+        $mform->closeHeaderBefore('instruction');
+
+        $listlink = self::print_backup_list_link(true);
+        $mform->addElement('html', $OUTPUT->heading($listlink, 5));
 
         $confirm = $OUTPUT->box(get_string('backup_confirm', 'local_devassist'), 'box py-3 modal-body');
         $mform->addElement('html', $confirm);
@@ -60,5 +97,20 @@ class backup extends \moodleform {
         $msg  = get_string('maintenance_warning', 'local_devassist');
         $html = $OUTPUT->notification($msg, \core\notification::WARNING);
         $mform->addElement('html', $html);
+    }
+
+    /**
+     * Print a link to the backup files list page.
+     * @param bool $return
+     * @return string|void
+     */
+    public static function print_backup_list_link($return = false) {
+        $label = get_string('backupfileslist', 'local_devassist');
+        $url = new \moodle_url('/local/devassist/backuplist.php');
+        $link = \html_writer::link($url, $label);
+        if ($return) {
+            return $link;
+        }
+        echo $link;
     }
 }
